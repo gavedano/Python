d0ea3d05df7ffef1a88b447e367e05453e1bb40a
Gonzalo Avedano <gonza.avedano@gmail.com>
2023-01-07 17:32:42 -0300

    primeros pasos

---
+++ 00_hello.py
@@ -0,0 +1,19 @@
+# Hola mundo
+
+"""
+Este es un comentario
+en varias
+lineas
+
+"""
+# Imprimir por pantalla: Con comillas simples o dobles es lo mismo
+print("Hola Python") 
+
+# Consultar el tipo de dato
+print(type("Hola Python")) # Tipo STR
+print(type(5)) # Tipo INT
+print(type(1.5)) # Tipo FLOAT
+print(type(True)) # Tipo BOOL
+
+
+
---
+++ 01_variables.py
@@ -0,0 +1,35 @@
+# Variables: Siempre en minusculas y en caso de nombres compuestos separo con _
+
+my_string_variable = "My String variable" # Guion bajo si, guion medio NO!
+print(my_string_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+# Con funcion STR() convierto el valor int a string (lo mismo con INT() SORTED() FLOAT() ENTRE OTRAS)
+my_int_to_str_variable = str(my_int_variable)
+print (my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+# Concatenar
+print(my_string_variable, my_int_variable, my_bool_variable) 
+print("Este es el valor de: ", my_bool_variable) # Concateno con texte pre-establecido
+# Funciones del sistema
+
+# LEN () : Cuenta el largo de caracteres
+print(len(my_int_to_str_variable)) 
+print(len(my_string_variable)) 
+
+# Variables en una sola linea
+name, surname, alias, age = "Gonzalo", "Avedano", "Zerone", 32
+print("Me llamo:", name, surname, "Mi edad es: ",age, "Y mi alias es: ", alias)
+
+# Solicitar al usuario el ingreso de datos y almacenarlos en variables (NO ES LO USUAL)
+first_name = input('Cual es tu nombre: ')
+age = input('Cuantos años tienes? ')
+
+print(first_name)
+print(age)
